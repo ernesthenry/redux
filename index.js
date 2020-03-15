0e609683cf5f3adacb81706b169781be4d8e47c0
ernesthenry <henry38ernest@gmail.com>
2020-03-15 11:14:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function  createStore() {
+    // 1. The store should have four parts
+    // 2. The state
+    // 3. Get the state
+    // 4. Listen to changes on the state
+    // 5. Update the state
+
+
+    let state
+
+    const getState  = () => state
+    
+}
