c1a558d31cf23caf4e3daa30b039d84c4f9a21e4
ernesthenry <henry38ernest@gmail.com>
2020-03-15 16:53:39 +0300

    Add goals reducer

--- index.js
+++ index.js
@@ -45,6 +45,19 @@ function todos(state = [],action) {
             Object.assign({}, todo, {complete: !todo.complete}))
         default:
             return state
+    }
+}
+
+function goals(state=[], action){
+    switch(action.type){
+        case ADD_GOAL:
+            return state.concat([action.goal])
+        case REMOVE_GOAL:
+            return state.filter((goal) => goal.id !== action.id)
+        default:
+            return state
+    }
+}
 
 const store =createStore(todos) // we pass our reducer function to create store so that we can add todos
 store.subscribe(()=>{
